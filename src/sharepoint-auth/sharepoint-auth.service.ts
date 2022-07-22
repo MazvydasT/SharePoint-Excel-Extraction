e8aabe3f45e03f958ddaba9b43cc32f962fe9d74
Maz T <mazvydas.tadaravicius@gmail.com>
2022-07-22 15:47:43 +0100

    Made getAuth use defer()

--- src/sharepoint-auth/sharepoint-auth.service.ts
+++ src/sharepoint-auth/sharepoint-auth.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { getAuth } from 'node-sp-auth';
-import { from } from 'rxjs';
+import { defer } from 'rxjs';
 
 @Injectable()
 export class SharePointAuthService {
 	getAuth(url: URL, username: string, password: string) {
-		return from(
-			getAuth(url.origin, {
+		return defer(() => {
+			return getAuth(url.origin, {
 				username,
 				password
-			})
-		);
+			});
+		});
 	}
 }
