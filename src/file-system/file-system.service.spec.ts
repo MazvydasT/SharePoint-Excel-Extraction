f6ccb7441df6c8529a28080f77b6c3ec070cb4bf
Maz T <mazvydas.tadaravicius@gmail.com>
2024-07-05 10:53:28 +0100

    Replaced End Of Line from CR to CRLF

--- src/file-system/file-system.service.spec.ts
+++ src/file-system/file-system.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { FileSystemService } from './file-system.service';
 
 describe('FileSystemService', () => {
-  let service: FileSystemService;
+	let service: FileSystemService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [FileSystemService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [FileSystemService]
+		}).compile();
 
-    service = module.get<FileSystemService>(FileSystemService);
-  });
+		service = module.get<FileSystemService>(FileSystemService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
